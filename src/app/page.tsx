470837fa7957f940aa6f9482988365f3f4c7f6fa
tayotitiloye <tolatayo@gmail.com>
2023-12-04 11:27:50 -0800

    gittest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>Git hub Training</h1>  
+      <h1>Git hub Training</h1>    
       <ul>
         {data.map(p => (
           <div key={p.id}>
